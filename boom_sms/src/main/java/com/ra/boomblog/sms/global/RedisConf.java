8ffdc4416831a29aa5f7fa983d30bfde77bfcaa6
HelloWorldKing1 <pra2010@163.com>
2022-08-16 17:33:07 +0800

    增加搜索和mq模块

---
+++ boom_sms/src/main/java/com/ra/boomblog/sms/global/RedisConf.java
@@ -0,0 +1,13 @@
+package com.ra.boomblog.sms.global;
+
+import com.ra.boomblog.base.global.BaseRedisConf;
+
+/**
+ * Redis相关常量
+ *
+ * @author 陌溪
+ * @date 2020年9月14日10:19:57
+ */
+public final class RedisConf extends BaseRedisConf {
+
+}
---
+++ boom_sms/src/main/java/com/ra/boomblog/sms/global/SysConf.java
@@ -0,0 +1,13 @@
+package com.ra.boomblog.sms.global;
+
+import com.ra.boomblog.base.global.BaseSysConf;
+
+/**
+ * 系统常量
+ *
+ * @author 陌溪
+ * @date 2018年11月3日13:13:07
+ */
+public final class SysConf extends BaseSysConf {
+
+}
